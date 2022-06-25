72f10d6462a83eaf7decdd4297038788989837a4
Gladys <gladysangelakamau@gmail.com>
2022-06-25 19:28:14 +0300

    step

--- Desktop/iNITIALpROJECT/index.js
+++ Desktop/iNITIALpROJECT/index.js
@@ -1,1 +1,4 @@
 const baseURL = "http://www.omdbapi.com/?i=tt3896198&apikey=99b3cbb&";
+const searchInput= document.getElementById("search-input");
+const moviePoster=document.getElementById("movie-poster")
+const movieTitle= document.getElementById("movie-title")
