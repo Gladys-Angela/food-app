53bf7642b390d2477e952a8752281a423af7f4e6
Gladys-Angela <gladysangelakamau@gmail.com>
2022-07-26 18:38:55 +0300

    firstcommit

---
+++ food-app/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className='bg-info'>this is food app</div>
+    </div>
+  );
+}
+
+export default App;
